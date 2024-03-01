0ed5c1edb44b02d00a3f3a70032323ffae4a84b5
i01011001 <yugen-m7>
2024-03-01 14:16:03 +0545

    fixed ws

--- websocket/main/websocket.c
+++ websocket/main/websocket.c
@@ -42,14 +42,14 @@ static esp_err_t uri_ws(httpd_req_t *req) {
    if (req->method == HTTP_GET)
        return ESP_OK;
 
-   httpd_ws_frame_t ws_frame = {
+   httpd_ws_frame_t ws_recv= {
        /* memset(&ws_frame, 0, sizeof(httpd_ws_frame_t)); */
        .type = HTTPD_WS_TYPE_TEXT,
        .payload = malloc(1024),
    };
-   httpd_ws_recv_frame(req, &ws_frame, 1024);
-   printf("ws payload: %.*s\n", ws_frame.len, ws_frame.payload);
-   free(ws_frame.payload);
+   httpd_ws_recv_frame(req, &ws_recv, 1024);
+   printf("ws payload: %s\n", ws_recv.payload);
+   free(ws_recv.payload);
 
    char *response = "connected";
    httpd_ws_frame_t ws_response = {.final = true,
@@ -59,30 +59,35 @@ static esp_err_t uri_ws(httpd_req_t *req) {
                                    .len = strlen(response)};
    return httpd_ws_send_frame(req, &ws_response);
 }
+
+
 void server() {
    httpd_handle_t httpd_handler = NULL;
    httpd_config_t httpd_config = HTTPD_DEFAULT_CONFIG();
+   httpd_start(&httpd_handler, &httpd_config);
 
    httpd_uri_t httpd_uri = {
        .uri = "/",
        .method = HTTP_GET,
        .handler = uri_test,
    };
+   httpd_register_uri_handler(httpd_handler, &httpd_uri);
+
    httpd_uri_t toggle_uri = {
        .uri = "/toggle",
        .method = HTTP_POST,
        .handler = uri_resp,
    };
+   httpd_register_uri_handler(httpd_handler, &toggle_uri);
+
    httpd_uri_t ws_uri = {
        .uri = "/ws",
        .method = HTTP_GET,
        .handler = uri_ws,
+       .is_websocket = true,
    };
-
-   httpd_start(&httpd_handler, &httpd_config);
-   httpd_register_uri_handler(httpd_handler, &httpd_uri);
-   httpd_register_uri_handler(httpd_handler, &toggle_uri);
    httpd_register_uri_handler(httpd_handler, &ws_uri);
+
 }
 
 void mdns_service() {
