887fd450127d286b14f09c761eb036758a5d981c
Yugen <yugen-m7>
2024-03-11 12:12:50 +0545

    nows prints version

--- ota/main/ota.c
+++ ota/main/ota.c
@@ -10,6 +10,7 @@
 #include <nvs_flash.h>
 #include <protocol_examples_common.h>
 #include <stdio.h>
+#include "esp_ota_ops.h"
 
 #define intr_button 0
 
@@ -17,15 +18,8 @@ SemaphoreHandle_t sema_handler;
 
 const int software_version = 1;
 
-typedef struct {
-  char *buffer;
-  int buffer_index;
-} chunk_payload_t;
-
-static char *buffer;
-
 // for https certification
-extern const uint8_t pem[] asm("_binary_drive_pem_start");
+extern const uint8_t cert_pem[] asm("_binary_drive_pem_start");
 
 void IRAM_ATTR intr_button_pushed() {
   xSemaphoreGiveFromISR(sema_handler, pdFALSE);
@@ -33,11 +27,11 @@ void IRAM_ATTR intr_button_pushed() {
 
 void intr_setup() {
   gpio_config_t intr_button_config = {
-      .intr_type = GPIO_INTR_POSEDGE,
-      .mode = GPIO_MODE_INPUT,
-      .pull_up_en = GPIO_PULLUP_ENABLE,
-      .pull_down_en = GPIO_PULLDOWN_DISABLE,
-      .pin_bit_mask = (1ULL << intr_button),
+    .intr_type = GPIO_INTR_POSEDGE,
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .pin_bit_mask = (1ULL << intr_button),
   };
   gpio_config(&intr_button_config);
   gpio_install_isr_service(0);
@@ -50,30 +44,32 @@ void wifi_setup() {
   esp_event_loop_create_default();
   example_connect();
 }
-esp_err_t client_event(esp_http_client_event_t *evt) { return ESP_OK; }
-
+esp_err_t client_event(esp_http_client_event_t *evt) { return ESP_OK; };
 void ota_setup() {
   while (1) {
     xSemaphoreTake(sema_handler, portMAX_DELAY);
+    wifi_setup();
+
+    printf("\n");
+    ESP_LOGI("STATUS", "Starting Update\n");
+
     esp_http_client_config_t client_config = {
-        .url = "https://drive.usercontent.google.com/"
-               "download?id=1eAgyp_c-KBP9P2bxWUN2RDhiFBNb9qn0&export=download&"
-               "authuser=0&confirm=t&uuid=e005ca17-41e0-446d-b570-7389a2a7c384&"
-               "at=APZUnTWM8gsfEjpUQAlRFv_F0fZ5:1710133243072",
-        .event_handler = client_event,
-        .cert_pem = (char *)pem};
-    esp_http_client_handle_t client_handle =
-        esp_http_client_init(&client_config);
-    esp_http_client_set_header(client_handle, "Content_type",
-                               "application/json");
+      .url = "https://drive.google.com/u/0/uc?id=1dbGPJuEjVX-0-c1mLOa-2iWjl2lDcFTR&export=download",
+      .event_handler = client_event,
+      .cert_pem = (char *)cert_pem};
+    esp_http_client_init(&client_config);
 
     esp_https_ota_config_t ota_config = {
-        .http_config = &client_config,
+      .http_config = &client_config,
     };
     if (esp_https_ota(&ota_config) == ESP_OK) {
       ESP_LOGI("UPDATE STATUS", "Update of Succesful");
-    }
+      printf("Restarting in 5 seconds");
+      vTaskDelay(pdMS_TO_TICKS(5000));
+      esp_restart();
+    }else{
     ESP_LOGE("UPDATE STATUS", "FAILED");
+    };
 
     // esp_http_client_perform(client_handle);
     // esp_http_client_cleanup(client_handle);
@@ -87,6 +83,12 @@ void app_main(void) {
   // configuration for isr push button
 
   intr_setup();
-  wifi_setup();
-  xTaskCreate(ota_setup, "function to be executed", 2048, NULL, 2, NULL);
+
+
+  const esp_partition_t *running_partition = esp_ota_get_running_partition();
+  esp_app_desc_t running_partition_description;
+  esp_ota_get_partition_description(running_partition, &running_partition_description);
+  printf("current firmware version is: %s\n", running_partition_description.version);
+
+  xTaskCreate(ota_setup, "function to be executed", 1024 * 8, NULL, 2, NULL);
 }
